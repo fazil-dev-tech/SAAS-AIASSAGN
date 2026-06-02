6fb50d27f823375ac4f6015ed62c79e8ab92090f
fazil-dev-tech <fazil-dev-tech@users.noreply.github.com>
2026-06-03 01:30:22 +0530

    Force trace chromium bin

--- assignai-next/next.config.mjs
+++ assignai-next/next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['@sparticuz/chromium', 'puppeteer-core'],
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/**/*': ['./node_modules/@sparticuz/chromium/bin/**/*']
+    }
+  }
 };
 
 export default nextConfig;
